37e5a6b72c5abad164f5da895cd8ce4a2fd6fed2
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-11 14:26:21 +0100

    register goes through validation, hashes password, creates user and sends back user to the user (without password)

--- src/controllers/user_controller.ts
+++ src/controllers/user_controller.ts
@@ -1,10 +1,11 @@
 /**
  * Controller Template
  */
+import bcrypt from 'bcrypt'
 import Debug from "debug"
 import { Request, Response } from "express"
-import { validationResult } from "express-validator"
-import prisma from "../prisma"
+import { matchedData, validationResult } from "express-validator"
+import { createUser } from '../services/user_service'
 
 // Create a new debug instance
 const debug = Debug("photo-album-api:user_controller")
@@ -17,7 +18,45 @@ export const login = async (req: Request, res: Response) => {}
 /**
  * Register a user
  */
-export const register = async (req: Request, res: Response) => {}
+export const register = async (req: Request, res: Response) => {
+	const validationErrors = validationResult(req)
+	if (!validationErrors.isEmpty()) {
+		return res.status(400).send({
+			status: "fail",
+			data: validationErrors.array()
+		})
+	}
+
+    const validatedData = matchedData(req)
+    const hashedPassword = await bcrypt.hash(validatedData.password, process.env.SALT_ROUNDS || 10)
+    validatedData.password = hashedPassword
+
+    const { email, password, first_name, last_name } = validatedData
+
+    try {
+        await createUser({
+            email,
+            password,
+            first_name,
+            last_name,
+        })
+
+        res.status(201).send({
+            status: "success",
+            data: {
+                email,
+                first_name,
+                last_name,
+            },
+        })
+    }
+    catch (err) {
+        res.status(500).send({
+            status: "error",
+            message: "Could not create user in database",
+        })
+    }
+}
 
 /**
  * Refresh access_token
