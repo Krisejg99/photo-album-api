14b463ab8571f663f80b684dfe4a546003e6cf2a
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-11 18:57:36 +0100

    getPhotos, getPhoto created

--- src/services/photo_service.ts
+++ src/services/photo_service.ts
@@ -2,3 +2,23 @@
  * Photo Services
  */
 import prisma from "../prisma"
+import { CreatePhotoData } from "../types"
+
+export const getPhotos = async () => {
+    return await prisma.photo.findMany()
+}
+
+export const getPhoto = async (photoId: number) => {
+    return await prisma.photo.findUniqueOrThrow({
+        where: {
+            id: photoId,
+        },
+        include: {
+            albums: true,
+        },
+    })
+}
+
+export const createPhoto = async (data: CreatePhotoData) => {
+    // return await prisma.photo.create({ data })
+}
