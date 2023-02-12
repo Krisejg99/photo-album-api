b7a4b9e966783f68f49c57447400e0c27c49e1a7
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-12 11:22:35 +0100

    getUserById defined

--- src/services/user_service.ts
+++ src/services/user_service.ts
@@ -5,11 +5,11 @@ import prisma from "../prisma"
 import { CreateUserData } from "../types"
 
 export const getUserByEmail = async (email: string) => {
-    return await prisma.user.findUnique({
-        where: {
-            email: email,
-        }
-    })
+    return await prisma.user.findUnique({ where: { email }})
+}
+
+export const getUserById = async (id: number) => {
+    return await prisma.user.findUnique({ where: { id }})
 }
 
 export const createUser = async (data: CreateUserData) => {
