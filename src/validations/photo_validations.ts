a638fdbb79648effb9ad25b5c5605105c9c7bd3b
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-13 12:18:46 +0100

    comments user "" while others user simple ones

--- src/validations/photo_validations.ts
+++ src/validations/photo_validations.ts
@@ -5,12 +5,12 @@ import { body } from 'express-validator'
 
 export const createPhotoValidations = [
     body('title')
-        .isString().withMessage('has to be a string').bail()
-        .isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
+        .isString().withMessage("has to be a string").bail()
+        .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
     body('url')
-        .isURL().withMessage('has to ba a URL'),
+        .isURL().withMessage("has to be a URL"),
     body('comment')
         .optional()
-        .isString().withMessage('has to be a string').bail()
-        .isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
+        .isString().withMessage("has to be a string").bail()
+        .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
 ]
--- src/validations/user_validations.ts
+++ src/validations/user_validations.ts
@@ -6,26 +6,26 @@ import { isValidEmail } from './custom_validations/user_custom_validations'
 
 export const createUserValidations = [
     body('email')
-        .isString().withMessage('has to be a string')
-        .isEmail().withMessage('has to be a valid email')
+        .isString().withMessage("has to be a string")
+        .isEmail().withMessage("has to be a valid email")
         .custom(isValidEmail),
     body('password')
-        .isString().withMessage('has to be a string')
-        .isLength({ min: 6 }).withMessage('has to be at least 6 chars long'),
+        .isString().withMessage("has to be a string")
+        .isLength({ min: 6 }).withMessage("has to be at least 6 chars long"),
     body('first_name')
-        .isString().withMessage('has to be a string')
-        .isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
+        .isString().withMessage("has to be a string")
+        .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
     body('last_name')
-        .isString().withMessage('has to be a string')
-        .isLength({ min: 3 }).withMessage('has to be at least 3 chars long'),
+        .isString().withMessage("has to be a string")
+        .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
 ]
 
 export const loginValidations = [
     body('email')
-        .isString().withMessage('has to be a string')
-        .isEmail().withMessage('has to be a valid email')
+        .isString().withMessage("has to be a string")
+        .isEmail().withMessage("has to be a valid email")
         .custom(isValidEmail),
     body('password')
-        .isString().withMessage('has to be a string')
-        .isLength({ min: 6 }).withMessage('has to be at least 6 chars long'),
+        .isString().withMessage("has to be a string")
+        .isLength({ min: 6 }).withMessage("has to be at least 6 chars long"),
 ]
