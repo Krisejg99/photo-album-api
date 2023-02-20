73509566e3f525ab9083b64a0bf91060757751a0
Kristopher Gray <kristopher.gray99@gmail.com>
2023-02-20 14:51:18 +0100

    .bail() user_validations

--- src/validations/user_validations.ts
+++ src/validations/user_validations.ts
@@ -6,18 +6,18 @@ import { isExistingEmail } from './custom_validations/user_custom_validations'
 
 export const createUserValidations = [
     body('email')
-        .isString().withMessage("has to be a string")
+        .isString().withMessage("has to be a string").bail()
         .isEmail().withMessage("has to be a valid email")
         .custom(isExistingEmail),
     body('password')
-        .isString().withMessage("has to be a string")
+        .isString().withMessage("has to be a string").bail()
         .isLength({ min: 6 }).withMessage("has to be at least 6 chars long"),
     body('first_name')
-        .isString().withMessage("has to be a string")
+        .isString().withMessage("has to be a string").bail()
         .trim()
         .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
     body('last_name')
-        .isString().withMessage("has to be a string")
+        .isString().withMessage("has to be a string").bail()
         .trim()
         .isLength({ min: 3 }).withMessage("has to be at least 3 chars long"),
 ]
